8c4cd6a4b874c68359c3676bffab0bb7af89db9d
Aditya Shah <AdityaShah@Adityas-MacBook-Pro-2.local>
2018-04-18 20:42:26 -0700

    Reformatted Conversation Data Structure

--- marketplace_backend/MarketPlaceStarter.sql
+++ marketplace_backend/MarketPlaceStarter.sql
@@ -1,43 +1,47 @@
 CREATE Database if not exists MarketPlace;
 Use MarketPlace;
 
+
+Drop Table if Exists Conversations;
 Drop Table if Exists SwipedCards;
 Drop Table if Exists Cards;
 Drop Table if Exists User;
 
 
 Create Table User (
-	userID int(11) not null AUTO_INCREMENT,
+    userID int(11) not null AUTO_INCREMENT,
     username varchar(50) not null,
     password varchar(50) not null,
     userJson json,
     primary key (userID) );
 
 Create Table Cards (
-	cardID int(11) not null AUTO_INCREMENT,
+    cardID int(11) not null AUTO_INCREMENT,
     itemJson json,
-    time timestamp not null,
+    time timestamp not null Default current_timestamp(),
     userID int(11) not null,
     primary key (cardID),
     constraint userID foreign key (userID) references User (userID) );
 
 Create Table SwipedCards (
-	usersID int(11) not null,
+    usersID int(11) not null,
     cardID int(11) not null,
     constraint usersID foreign key (usersID) references User (userID),
     constraint cardID foreign key (cardID) references Cards (cardID) );
 
 Create Table Conversations (
-    buyerID int(11) not null,
-    sellerID int(11) not null,
-    messagesJson json,
-    time timestamp not null;
-    constraint buyerID foreign key (buyerID) references User (userID),
-    constraint sellerID foreign key (sellerID) references User (userID);
-)
+    user_id_1 int(11) not null,
+    user_id_2 int(11) not null,
+    cardID int(11) not null,
+    cardName varchar(50) not null,
+    messagesJson text,
+    numberOfMessages int(11),
+    time timestamp not null Default current_timestamp(),
+    constraint user_id_1 foreign key (user_id_1) references User (userID),
+    constraint user_id_2 foreign key (user_id_2) references User (userID) );
 
 
-	INSERT INTO User (username, password) values ('Heermann', 'Andrew'), ('Shah', 'Aditya' ), ('Chan', 'Allison'), ('Hashemi', 'Ali');
+    INSERT INTO User (username, password) values ('Heermann', 'Andrew'), ('Shah', 'Aditya' ), ('Chan', 'Allison'), ('Hashemi', 'Ali');
 
     Insert into Cards (itemJson, time, userID) values
     ('{
@@ -65,5 +69,12 @@ Create Table Conversations (
 
     INSERT INTO SwipedCards (usersID, cardID) values (1, 1);
 
-    INSERT INTO Conversations (buyerID, sellerID, time) values
-    (1, 2, current_timestamp())
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (1, 3, 1, 'Car');
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (3, 1, 1, 'Car');
+
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (1, 2, 3, 'PS4');
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (2, 1, 3, 'PS4');
+    
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (2, 1, 2, 'Unused Iphone X');
+    INSERT INTO Conversations (user_id_1, user_id_2, cardID, cardName) values (1, 2, 2, 'Unused Iphone X');
+
