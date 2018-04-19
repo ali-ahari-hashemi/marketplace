a509acb5c31a19f9e241fd0ad9d533c2e40f98af
aheermann <Andrew@AndrewHs-MacBook-Pro.local>
2018-04-19 00:57:39 -0700

    Get Conversations

--- marketplace_server/src/JavaPrograms/src/messages/GetConversations.java
+++ marketplace_server/src/JavaPrograms/src/messages/GetConversations.java
@@ -1,4 +1,4 @@
-package test;
+package messages;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 //input userid
 //output json of conversations
-public class test {
+public class GetConversations {
 
 	public static void main(String [] args) {
 		Connection conn = null;
